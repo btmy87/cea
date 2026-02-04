99954f2e318c6e30f5a5874d0d02ed8d1f68faf8
Brian Magaha <btmy87@gmail.com>
2026-02-04 18:47:13 -0500

    Initial cut at native matlab interface.
    Implemented enough of a native matlab interface to complete
    rp1311_example1.  Need to hack on header file to switch char* [] to
    char**.  This hack isn't checked in, need to find a good way to
    implement.

---
+++ source/bind/matlab/CEA.m
@@ -0,0 +1,111 @@
+classdef CEA
+    properties
+        alias = "cea";
+    
+    end
+    
+    methods
+        function obj = CEA()
+            % Create 
+            if ~libisloaded(obj.alias)
+                load_cea();
+            end
+        end
+
+        function set_log_level(obj, logLevel)
+            arguments
+                obj
+                logLevel (1, 1) string {mustBeMember(logLevel, ...
+                    ["CEA_LOG_CRITICAL", "CEA_LOG_ERROR", ...
+                     "CEA_LOG_WARNING", "CEA_LOG_INFO", ...
+                     "CEA_LOG_DEBUG", "CEA_LOG_NONE"])} = ...
+                     "CEA_LOG_CRITICAL";
+            end
+            err = calllib(obj.alias, "cea_set_log_level", char(logLevel));
+            assert(err == "CEA_SUCCESS");
+        end
+
+        function mix = mixture_create(obj, reactants)
+            arguments
+                obj
+                reactants (1, :) string;
+            end
+            mix = libpointer("voidPtr", 0);
+            n = length(reactants);
+            % reac = libpointer("cstring", cellstr(reactants));
+            reac = libpointer("stringPtrPtr", cellstr(reactants));
+            err = calllib(obj.alias, "cea_mixture_create", ...
+                mix, n, reac);
+            assert(err == "CEA_SUCCESS", "Error in cea_mixture_create");
+        end
+           
+        function opts = solver_opts_init(obj)
+            opts = libpointer("cea_solver_opts", struct());
+            err = calllib(obj.alias, "cea_solver_opts_init", opts);
+            assert(err == "CEA_SUCCESS", "Error in cea_solver_opts_init");
+        end
+
+        function solver = eqsolver_create_with_options(obj, prod, opts)
+            solver = libpointer("voidPtr", 0);
+            err = calllib(obj.alias, "cea_eqsolver_create_with_options", ...
+                solver, prod, opts);
+            assert(err == "CEA_SUCCESS", "Error in eqsolver_create_with_options");
+        end
+
+        function solution = eqsolution_create(obj, solver)
+            solution = libpointer("voidPtr", 0);
+            err = calllib(obj.alias, "cea_eqsolution_create", ...
+                solution, solver);
+            assert(err == "CEA_SUCCESS", "Error in cea_eqsolution_create");
+        end
+
+        function partials = eqpartials_create(obj, solver)
+            partials = libpointer("voidPtr", 0);
+            err = calllib(obj.alias, "cea_eqpartials_create", ...
+                partials, solver);
+            assert(err == "CEA_SUCCESS", "Error in cea_eqpartialscreate");
+        end
+
+        function weights = mixture_moles_to_weights(obj, reac, n, moles)
+            weights = libpointer("doublePtr", zeros(1, n));
+            err = calllib(obj.alias, "cea_mixture_moles_to_weights", ...
+                reac, n, moles, weights);
+            assert(err == "CEA_SUCCESS", "Error in cea_mixture_moles_to_weights");
+        end
+
+        function OFRatio = mixture_chem_eq_ratio_to_of_ratio(...
+                obj, reac, n, oxidWeights, fuelWeights, er)
+            ofptr = libpointer("doublePtr", 0);
+            err = calllib(obj.alias, "cea_mixture_chem_eq_ratio_to_of_ratio", ...
+                reac, n, oxidWeights, fuelWeights, er, ofptr);
+            assert(err == "CEA_SUCCESS", "Error in cea_mixture_chem_eq_ratio_to_of_ratio");
+            OFRatio = ofptr.Value;
+        end
+
+        function weights = mixture_of_ratio_to_weights(...
+                obj, reac, n, oxidWeights, fuelWeights, OFRatio)
+            weightsPtr = libpointer("doublePtr", zeros(1, n));
+            err = calllib(obj.alias, "cea_mixture_of_ratio_to_weights", ...
+                reac, n, oxidWeights, fuelWeights, OFRatio, weightsPtr);
+            assert(err == "CEA_SUCCESS", "Error in cea_mixture_of_ratio_to_weights");
+            weights = weightsPtr.Value;
+        end
+
+        function eqsolver_solve_with_partials(obj, solver, prob, ...
+                state1, state2, weights, soln, partials)
+            err = calllib(obj.alias, "cea_eqsolver_solve_with_partials", ...
+                solver, prob, state1, state2, weights, soln, partials);
+            assert(err == "CEA_SUCCESS", "Error in cea_eqsolver_solve_with_partials");
+        end
+
+        function out = eqsolution_get_property(obj, soln, prop)
+            outPtr = libpointer("doublePtr", 0.0);
+            err = calllib(obj.alias, "cea_eqsolution_get_property", ...
+                soln, prop, outPtr);
+            assert(err == "CEA_SUCCESS", "Error in cea_eqsolution_get_property");
+            out = outPtr.Value; 
+        end
+    end
+
+
+end
---
+++ source/bind/matlab/samples/rp1311_example1.m
@@ -0,0 +1,79 @@
+%% rp1311_example1.m
+% recreate rp1311_example1.c
+close all
+clear
+clc
+
+%% Load CEA
+load_cea();
+cea = CEA();
+cea.set_log_level("CEA_LOG_CRITICAL"); % lack of a console causes a crash if it actually writes a log
+                                       % LOG_NONE doesn't do what you'd expect
+
+%% Define Problem
+reactants = ["H2", "Air"];
+fuel_moles = [1.0, 0.0];
+oxidant_moles = [0.0, 1.0];
+products = ["Ar",    "C",    "CO",   "CO2",  "H", ...
+            "H2",    "H2O",  "HNO",  "HO2",  "HNO2", ...
+            "HNO3",  "N",    "NH",   "NO",   "N2", ...
+            "N2O3",  "O",    "O2",   "OH",   "O3"];
+
+pressures = [1.0, 0.1, 0.01].*1.01325;
+temperatures = [3000.0, 2000.0];
+chem_eq_ratios = [1.0, 1.5];
+
+% create mixtures
+reac = cea.mixture_create(reactants);
+prod = cea.mixture_create(products);
+
+% create solver
+opts = cea.solver_opts_init();
+opts.Value.reactants = reac;
+solver = cea.eqsolver_create_with_options(prod, opts);
+
+% equilibrium solution
+soln = cea.eqsolution_create(solver);
+
+% partials
+partials = cea.eqpartials_create(solver);
+
+% unit conversions
+fuel_weights = cea.mixture_moles_to_weights(reac, ...
+    length(reactants), fuel_moles);
+oxidant_weights = cea.mixture_moles_to_weights(reac, ...
+    length(reactants), oxidant_moles);
+
+of_ratios = zeros(size(chem_eq_ratios));
+for i = 1:length(of_ratios)
+    of_ratios(i) = cea.mixture_chem_eq_ratio_to_of_ratio( ...
+        reac, length(reactants), oxidant_weights, fuel_weights, ...
+        chem_eq_ratios(i));
+end
+
+% equilibrium solve
+fprintf("%10s  %10s  %10s  %10s  %12s  %12s  %12s  %12s\n", ...
+        "T (K)", "P (Pa)", "Chem Equiv", "O/F Ratio", ...
+        "H2 (wtfrac)", "Air (wtfrac)", "H (cal/g)", "Cp (cal/g-K)");
+
+for ir = 1:length(of_ratios)
+    weights = cea.mixture_of_ratio_to_weights(reac, length(reactants), ...
+        oxidant_weights, fuel_weights, of_ratios(ir));
+
+    for ip = 1:length(pressures)
+        for it = 1:length(temperatures)
+            cea.eqsolver_solve_with_partials(solver, "CEA_TP", ...
+                temperatures(it), pressures(ip), weights, soln, partials);
+            h = cea.eqsolution_get_property(soln, "CEA_ENTHALPY");
+            cp = cea.eqsolution_get_property(soln, "CEA_EQUILIBRIUM_CP");
+            h = h/4.184;
+            cp = cp/4.184;
+
+            fprintf("%10.2f  %10.2f  %10.2f  %10.6f  %12.5e  %12.5e  %12.5e  %12.5e\n", ...
+                temperatures(it), pressures(ip), chem_eq_ratios(ir), ...
+                of_ratios(ir), weights(1), weights(2), h, cp);
+        end
+    end
+end
+
+
